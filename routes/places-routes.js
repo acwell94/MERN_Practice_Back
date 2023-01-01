16d54319d505eecf874199bc9c2fb4e36784da6d
acwell94 <acwell94@naver.com>
2023-01-01 22:50:44 +0900

    post route 추가 및 404에러 추가(경로가 없을 시)

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,43 +1,12 @@
 const express = require("express");
 
-const router = express.Router();
-const HttpError = require("../models/http-error");
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+const placesControllers = require("../controllers/places-controller");
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; // {pid: 'p1'}
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-  res.json({ place });
-});
-router.get("/user/:uid", (req, res, next) => {
-  console.log(req.params, "VeryCool");
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-  if (!place) {
-    // next는 반드시 return 해주야 됨 비동기 적인 처리
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    );
-  }
-  res.json({ place });
-});
+const router = express.Router();
+
+router.get("/:pid", placesControllers.getPlaceById);
+
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
+
+router.post("/", placesControllers.createPlace);
 module.exports = router;
